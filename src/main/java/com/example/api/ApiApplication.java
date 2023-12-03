edbbc5e9311794b4cd7deba5130c290b29a9c495
Serj <serj053@yandex.ru>
2023-12-04 00:41:34 +0300

    Add antity Student

--- src/main/java/com/example/api/ApiApplication.java
+++ src/main/java/com/example/api/ApiApplication.java
@@ -2,12 +2,24 @@ package com.example.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class ApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
+	}
+	@GetMapping(path="students")
+	public List<Student> list(){
+		return List.of(new Student("Alex", LocalDate.of(2000, Month.JANUARY, 1)),
+				new Student("Tom", LocalDate.of(2002, Month.MARCH, 6)));
 	}
 
 }
---
+++ src/main/java/com/example/api/Student.java
@@ -0,0 +1,26 @@
+package com.example.api;
+
+import java.time.LocalDate;
+
+public class Student {
+    private Long id;
+    private String name;
+    private LocalDate dob;
+
+    public Student(String name, LocalDate dob) {
+        this.name = name;
+        this.dob = dob;
+    }
+
+    public Student() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+}
