ba90d93a11adfa150cdbddbf5be0f043b1b3df0b
Serj <serj053@yandex.ru>
2023-12-05 20:49:50 +0300

    Add class Studentconfiguration with methods CommandLine

---
+++ src/main/java/com/example/api/students/StudentConfiguration.java
@@ -0,0 +1,23 @@
+package com.example.api.students;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfiguration {
+
+    @Bean
+    public CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
+        return args -> {
+            studentRepository.saveAll(List.of(
+                    new Student("Alex", LocalDate.of(2000, Month.JANUARY, 1)),
+                    new Student("Tom", LocalDate.of(2002, Month.MARCH, 6))
+            ));
+        };
+    }
+}
