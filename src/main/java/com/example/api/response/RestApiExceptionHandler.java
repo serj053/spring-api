ed9b56f7b38fb4460ba6ae9af01fb26c65859be3
Serj <serj053@yandex.ru>
2023-12-06 13:41:49 +0300

    переключаем аннотацию @ExceptionHandler()  для использования исключения в режиме для всех

--- src/main/java/com/example/api/response/RestApiExceptionHandler.java
+++ src/main/java/com/example/api/response/RestApiExceptionHandler.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class RestApiExceptionHandler extends RuntimeException{
-    @ExceptionHandler(value = RestApiException.class)//эта аннотация будет использована только на RestAp[iException
+    @ExceptionHandler()
     public ResponseEntity<Object> handleException(Exception e) {
         HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
         return new ResponseEntity<>(e.getMessage(), status);
