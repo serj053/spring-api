930cc798c7a821bc6bc2dc64f899ae999442727b
Serj <serj053@yandex.ru>
2023-12-06 13:52:55 +0300

    исправлена ошибка - пропущены фигурные скобки @ExceptionHandler(value = {RestApiException.class})

--- src/main/java/com/example/api/response/RestApiExceptionHandler.java
+++ src/main/java/com/example/api/response/RestApiExceptionHandler.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
-public class RestApiResponseHandler extends RuntimeException{
-    @ExceptionHandler()
+public class RestApiExceptionHandler extends RuntimeException{
+    @ExceptionHandler(value = {RestApiException.class})//эта аннотация будет использована только на RestAp[iException
     public ResponseEntity<Object> handleException(Exception e) {
         HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
         return new ResponseEntity<>(e.getMessage(), status);
